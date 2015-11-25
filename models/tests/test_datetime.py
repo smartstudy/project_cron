df058e36c425473a2915d1bdada4d4f4220361d6
ecleya <ecleya@gmail.com>
2015-11-25 16:11:01 +0900

    시간을 좀 자유롭게 편집하기 위해 DateTime이라는 클래스 생성

---
+++ models/tests/test_datetime.py
@@ -0,0 +1,19 @@
+import unittest
+from models.datetime import DateTime
+
+
+class TestDateTime(unittest.TestCase):
+    def test_init_with_specific_hour(self):
+        datetime = DateTime(hour=3)
+
+        self.assertEqual(datetime._datetime.hour, 3)
+
+    def test_to_string(self):
+        datetime = DateTime(year=2015, month=11, day=25, hour=16, minute=7)
+
+        self.assertEqual(str(datetime), '2015-11-25 16:07')
+
+    def test_from_string(self):
+        datetime = DateTime.from_string('2015-11-25 16:07')
+
+        self.assertEqual(datetime, DateTime(year=2015, month=11, day=25, hour=16, minute=7))
