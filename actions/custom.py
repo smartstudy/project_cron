e3cf9524e81a28310b9f3d2687af34e1a6aede4d
ecleya <ecleya@smartstudy.co.kr>
2015-12-16 19:40:22 +0900

    버그 수정

--- actions/custom.py
+++ actions/custom.py
@@ -4,7 +4,8 @@ from utils import processutil, logutil
 def call(parameters):
     command = parameters["command"]
     cwd = parameters.get('cwd', None)
-    out, err = processutil.call(command, cwd=cwd)
+    ret_code, out, err = processutil.call(command, cwd=cwd)
 
+    logutil.info('Return Code', str(ret_code))
     logutil.info('Output', out)
     logutil.info('Error', err)
